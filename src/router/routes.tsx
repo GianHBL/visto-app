7097dacb1458c6c23637b1ba53ac51b24c255222
GianHBL <gian.lopes@ufv.br>
2023-12-08 17:45:43 -0300

    front-commit

---
+++ src/router/routes.tsx
@@ -0,0 +1,25 @@
+//Basic Imports
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+
+//Pages Imports
+import Welcome from '../pages/welcome/welcome';
+import Login from '../pages/login/login';
+import Register from '../pages/register/register';
+import Tabs from './tabs';
+
+
+//Constants
+const Stack = createNativeStackNavigator();
+
+export default function Routes() {
+    return (
+        <Stack.Navigator>
+            <Stack.Screen name="Welcome" component={Welcome} options={{ headerShown: false }} />
+            <Stack.Screen name="Login" component={Login} options={{ headerShown: false }} />
+            <Stack.Screen name="Register" component={Register} options={{ headerShown: false }} />
+            <Stack.Screen name="Tabs" component={Tabs} options={{ headerShown: false }} />
+        </Stack.Navigator>
+    );
+}
+
